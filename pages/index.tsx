47c5838a372ec9c0f27f2d971b35a3f27cfbcf2e
Aleksey Gusev <audetv@gmail.com>
2021-08-20 22:34:29 +0300

    Added html props to buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,13 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Hello World!</Htag>
-      <Button appearance={'primary'}>Кнопка</Button>
+      <Button
+        appearance={'primary'}
+        className={'test'}
+        onClick={() => console.log('here')}
+      >
+        Кнопка
+      </Button>
       <Button appearance={'ghost'}>Кнопка</Button>
     </>
   );
