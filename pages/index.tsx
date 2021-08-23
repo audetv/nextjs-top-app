37ddf8b85f21cdf2a88a7b4ec25f193672c4c5cd
Aleksey Gusev <audetv@gmail.com>
2021-08-23 22:26:37 +0300

    Added eslint-plugin-react-hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import { Button, Htag, P, Tag } from '../components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home(): JSX.Element {
   const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    console.log('Counter ' + counter);
+    return function cleanup() {
+      console.log('Unmount');
+    };
+  });
+
   return (
     <>
       <Htag tag="h1">Hello World! {counter}</Htag>
