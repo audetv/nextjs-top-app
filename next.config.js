87260d897dfa1d1c70c70bd8e6d51765c202371a
Aleksey Gusev <audetv@gmail.com>
2021-08-24 13:18:57 +0300

    Added images.d.ts

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
+  webpack(config, options) {
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      test: /\.svg$/i,
+      issuer: { and: [/\.(ts)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
     });
+
     return config;
   },
 };
