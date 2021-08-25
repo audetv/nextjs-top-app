4e95485b747febd43d4d9a9d12819228055e87e8
Aleksey Gusev <audetv@gmail.com>
2021-08-26 00:37:30 +0300

    Added menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import { Button, Htag, P, Rating, Tag } from '../components';
 import { useEffect, useState } from 'react';
 import { withLayout } from '../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import * as process from 'process';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
   const [counter, setCounter] = useState<number>(0);
 
   const [rating, setRating] = useState<number>(4);
@@ -55,8 +59,32 @@ function Home(): JSX.Element {
         isEditable={true}
         setRating={setRating}
       />
+      <ul>
+        {menu.map((m: MenuItem) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    { firstCategory }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
