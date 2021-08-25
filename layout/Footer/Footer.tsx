402322e830071a9fcfd7a4819f6be36b22135a3a
Aleksey Gusev <audetv@gmail.com>
2021-08-25 15:58:40 +0300

    Added footer styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,5 +1,18 @@
 import { FooterProps } from './Footer.props';
+import styles from './Footer.module.scss';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(styles.footer, className)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href={'#'} target={'_blank'} rel="noreferrer">
+        Пользовательское соглашение
+      </a>
+      <a href={'#'} target={'_blank'} rel="noreferrer">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
