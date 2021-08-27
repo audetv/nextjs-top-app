fd5a0a0fd7ffb885e32cf07bb04d1a8f372b26b6
Aleksey Gusev <audetv@gmail.com>
2021-08-27 16:09:20 +0300

    Changed Sidebar, added styles, logo

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { SidebarProps } from './Sidebar.props';
 import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
+import cn from 'classnames';
+import styles from './Sidebar.module.scss';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>Поиск</div>
       <Menu />
     </div>
   );
