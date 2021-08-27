5a52b13f45e50a5656490505b71c196a8ce4f9fa
Aleksey Gusev <audetv@gmail.com>
2021-08-27 17:54:40 +0300

    Changed Top Page Component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,33 @@
 import { TopPageComponentProps } from './TopPageComponent.props';
+import { Htag, Tag } from '../../components';
+import styles from './TopPageComponent.module.css';
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps) => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag={'h1'}>{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортировка</span>
+      </div>
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag={'h2'}>Вакансии - {page.category}</Htag>
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
+      </div>
+      <div className={styles.hh}></div>
+    </div>
+  );
 };
